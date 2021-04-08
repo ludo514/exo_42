868cfb8b1c6681e877daec24af04923c1a825a98
ludo <ludovict40@gmail.com>
2021-04-09 00:17:17 +0200

    modif

--- ft_sqrt.py
+++ ft_sqrt.py
@@ -11,6 +11,7 @@ def ft_sqrt(nombre):
         print("Erreur valeur")
 
 nombre = input("Calculé la racine carré de : ")
-if int(nombre) != None and ft_sqrt(int(nombre)) != None:
-    print(ft_sqrt(int(nombre)))
+valeur = ft_sqrt(int(nombre))
+if int(nombre) != None and valeur != None:
+    print(valeur)
     print(time.process_time())
