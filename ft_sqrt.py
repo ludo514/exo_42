a3dc140ff2f93e98134599e51db6ff05ba6d355c
ludo <ludovict40@gmail.com>
2021-04-09 00:14:45 +0200

    add sqrt

--- ft_sqrt.py
+++ ft_sqrt.py
@@ -1,5 +1,16 @@
-def ft_sqrt(nombre):
-    if int(nombre) != None:
-        pass
+import time
 
-print(ft_sqrt(9))
+def ft_sqrt(nombre):
+    resultat = 0
+    try:
+        for i in range(nombre):
+            resultat = i * i
+            if resultat == nombre:
+                return i
+    except TypeError:
+        print("Erreur valeur")
+
+nombre = input("Calculé la racine carré de : ")
+if int(nombre) != None and ft_sqrt(int(nombre)) != None:
+    print(ft_sqrt(int(nombre)))
+    print(time.process_time())
